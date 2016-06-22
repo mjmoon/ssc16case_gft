889cf33c22c428c066e962c62adc700b371bc699
mjmoon <michael.jongho.moon@gmail.com>
2016-06-22 02:25:46 -0400

    Finalized for blog posting
    Finalized for blog posting

--- cvPlot.R
+++ cvPlot.R
@@ -13,34 +13,29 @@ bc.res <- rbind(cvloop(bc, type = "AR", arpdq = c(2,1,0), arpdqs = c(1,1,0)),
 ab.res <- rbind(cvloop(ab, type = "AR", arpdq = c(2,1,0), arpdqs = c(1,1,0)),
                 cvloop(ab, type = "DR"))
 
-armethods <- c("AR2", "AR3", "AR4")
-drmethods <- c("DR20", "DR22", "DR30", "DR33", "DR40", "DR44")
+# armethods <- c("AR2", "AR3", "AR4")
+# drmethods <- c("DR20", "DR22", "DR30", "DR33", "DR40", "DR44")
 
-png("Img/arCanada_bar.png", width = pltWidth, height = pltHeight)
-ggplot(data = subset(can.res, index == 1 & method %in% armethods), 
+png("Img/cvCan.png", width = pltWidth, height = pltHeight)
+ggplot(data = subset(can.res, index == 1), 
        aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Cross-validation MSE\n\nCanada") +
-  geom_bar(stat = "identity", width = 0.2) +
-  theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0))) +
-  scale_y_continuous(labels = format1000)
-dev.off()
-
-png("Img/drCanada_bar.png", width = pltWidth, height = pltHeight)
-ggplot(data = subset(can.res, index == 1 & method %in% drmethods), 
-       aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Cross-validation MSE\n\nCanada") +
+  labs(x = "", y = "Cross-validation MSE (1,000s)", 
+       title = "Cross-validation MSEs\n\nCanada") +
   geom_bar(stat = "identity", width = 0.2) +
   theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0)))+
   scale_y_continuous(labels = format1000)
 dev.off()
 
-plt.dr.on <- ggplot(data = subset(on.res, index == 1 & method %in% drmethods), 
+plt.on <- ggplot(data = subset(on.res, index == 1), 
                  aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Ontario") +
+  labs(x = "", y = "Cross-validation MSE (1,000s)", title = "Ontario") +
   geom_bar(stat = "identity", width = 0.2) +
   theme(axis.title.y=element_text(margin=margin(0,20,0,0))) + 
   scale_fill_discrete(guide = guide_legend(ncol = 1, title = ""),
-                      labels = c("DR20: Dynamic regression with 2-week lead",
+                      labels = c("AR2: 2-week forecasts from Seasonal ARIMA",
+                                 "AR3: 3-week forecasts from Seasonal ARIMA",
+                                 "AR4: 4-week forecasts from Seasonal ARIMA",
+                                 "DR20: Dynamic regression with 2-week lead",
                                  "DR22: Dynamic regression with 2-week lead/GFT",
                                  "DR30: Dynamic regression with 3-week lead",
                                  "DR33: Dynamic regression with 3-week lead/GFT",
@@ -48,58 +43,26 @@ plt.dr.on <- ggplot(data = subset(on.res, index == 1 & method %in% drmethods),
                                  "DR44: Dynamic regression with 4-week lead/GFT")) +
   scale_y_continuous(labels = format1000)
 
-plt.dr.legend <- get_legend(plt.dr.on)
+plt.legend <- get_legend(plt.on)
 
-plt.dr.bc <- ggplot(data = subset(bc.res, index == 1 & method %in% drmethods), 
+plt.bc <- ggplot(data = subset(bc.res, index == 1), 
                  aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "British Columbia") +
+  labs(x = "", y = "Cross-validation MSE (1,000s)", title = "British Columbia") +
   geom_bar(stat = "identity", width = 0.2) +
   theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0))) +
   scale_y_continuous(labels = format1000)
 
-plt.dr.ab <- ggplot(data = subset(ab.res, index == 1 & method %in% drmethods), 
+plt.ab <- ggplot(data = subset(ab.res, index == 1), 
                  aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Alberta") +
+  labs(x = "", y = "Cross-validation MSE (1,000s)", title = "Alberta") +
   geom_bar(stat = "identity", width = 0.2) +
   theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0))) +
   scale_y_continuous(labels = format1000)
 
-png("Img/drProvinces_bar.png", width = pltWidth, height = pltWidth)
-grid.arrange(plt.dr.on + theme(legend.position = "none"), plt.dr.bc, plt.dr.ab, plt.dr.legend,
+png("Img/cvProv.png", width = pltWidth, height = pltWidth)
+grid.arrange(plt.on + theme(legend.position = "none"), plt.bc, plt.ab, plt.legend,
              ncol = 2, nrow = 2)
 dev.off()
 
-plt.ar.on <- ggplot(data = subset(on.res, index == 1 & method %in% armethods), 
-                    aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Ontario") +
-  geom_bar(stat = "identity", width = 0.2) +
-  theme(axis.title.y=element_text(margin=margin(0,20,0,0))) + 
-  scale_fill_discrete(guide = guide_legend(ncol = 1, title = ""),
-                      labels = c("AR2: SARIMA 2-week forecast",
-                                 "AR3: SARIMA 3-week forecast",
-                                 "AR4: SARIMA 4-week forecast")) +
-  scale_y_continuous(labels = format1000)
-
-plt.ar.legend <- get_legend(plt.ar.on)
-
-plt.ar.bc <- ggplot(data = subset(bc.res, index == 1 & method %in% armethods), 
-                    aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "British Columbia") +
-  geom_bar(stat = "identity", width = 0.2) +
-  theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0))) +
-  scale_y_continuous(labels = format1000)
-
-plt.ar.ab <- ggplot(data = subset(ab.res, index == 1 & method %in% armethods), 
-                    aes(x = method, y = meanMSE, fill = method)) + mytheme +
-  labs(x = "", y = "Cross-validation MSE (1,000)", title = "Alberta") +
-  geom_bar(stat = "identity", width = 0.2) +
-  theme(legend.position = "none", axis.title.y=element_text(margin=margin(0,20,0,0))) +
-  scale_y_continuous(labels = format1000)
-
-png("Img/arProvinces_bar.png", width = pltWidth, height = pltWidth)
-grid.arrange(plt.ar.on + theme(legend.position = "none"), plt.ar.bc, plt.ar.ab, plt.ar.legend,
-             ncol = 2, nrow = 2)
-dev.off()
-
-save(can.res, on.res, bc.res, ab.res, file = "Res.RData")
+# save(can.res, on.res, bc.res, ab.res, file = "Res.RData")
 # load("Res/Res.RData")
--- predPlot.R
+++ predPlot.R
@@ -2,6 +2,7 @@ source("importData.R")
 source("graphicSetting.R")
 library(dynlm)
 library(dyn)
+library(forecast)
 
 minYr <- as.numeric(format(minDate, '%Y'))
 minWk <- as.numeric(format(minDate + 3, '%U'))
@@ -13,6 +14,7 @@ ts.can.valid <- window(ts.flupos, start = c(2013, 1))
 
 # SARIMA prediction
 ar.model <- arima(ts.can.learn, c(2,1,0), list(order = c(1,1,0)))
+# ar.model <- arima(ts.can.learn, c(1,0,0), list(order = c(0,0,0)))
 summary(ar.model)
 ar.pred <- NULL
 lsize <- length(ts.can.learn)
@@ -20,15 +22,15 @@ for(k in 1:(length(ts.can.valid))){
   ar.pred.loop <- forecast(Arima(
     c(ts.can.learn[min(0,lsize - k): lsize], ts.can.valid[1:k]), model = ar.model), h = 3)
   ar.pred <- rbind(ar.pred, 
-                   c(fit = unlist(ar.pred.loop$mean[3]),
-                     LL = unlist(ar.pred.loop$lower[3,2]), 
-                     UL = unlist(ar.pred.loop$upper[3,2])))
+                   c(unlist(ar.pred.loop$mean[3]),
+                     unlist(ar.pred.loop$lower[3,2]), 
+                     unlist(ar.pred.loop$upper[3,2])))
 }
 
 ar.pred <- as.data.frame(ar.pred)
+names(ar.pred) <- c("fit", "LL", "UL")
 ar.pred$Date <- can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)]
 ar.pred$FluPos <- can$FluPos[(length(can$FluPos)-length(ts.can.valid)+1) : length(can$FluPos)]
-names(ar.pred)[2:3] <- c("LL", "UL")
 
 # SARIMA forecast
 ar.forec <- forecast(ar.model, h = length(ts.can.valid))
@@ -40,30 +42,12 @@ ar.forec.df$Date <- ar.pred$Date
 ar.forec.df$FluPos <- ar.pred$FluPos
 ar.forec.df <- as.data.frame(ar.forec.df)
 
-# dynamic regression prediction
-ts.gft <- ts(can$gft, frequency = 52, start = c(minYr, minWk))
-ts.gft.can.learn <- window(ts.gft, start = c(minYr, minWk), 
-                       end = c(2012, 52))
-ts.gft.can.valid <- window(ts.gft, start = c(2013, 1))
-
-dr.learn.dt <- data.frame(gft = ts.gft.can.learn, FluPos = ts.can.learn)
-dr.valid.dt <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid)
-
-dr.model <- dynlm(FluPos ~ L(FluPos, 3) + L(gft, 3) + L(FluPos, 52), data = dr.learn.dt)
-
-dr.pred <- predict(dr.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
-dr.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
-                       fit = dr.pred$fit[,"fit"], 
-                       UL = dr.pred$fit[,"upr"], 
-                       LL = dr.pred$fit[,"lwr"], 
-                       Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
-
 # plots
 png("Img/arForec.png", width = pltWidth, height = pltHeight)  
 ggplot(data = ar.forec.df, aes(x = Date, y = fit)) + 
   mytheme +
-  labs(x = "Year", y = "Frequency (1,000)", title = "Two-year forecasts with SARIMA") +
-  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% confidence interval")) +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "Two-year forecasts with SARIMA") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
   geom_line(aes(y = FluPos, col = "Actual")) +
   geom_line(aes(col = "Forecasted"))  + 
   scale_fill_brewer(name = "") + 
@@ -75,8 +59,8 @@ dev.off()
 png("Img/arPred.png", width = pltWidth, height = pltHeight)  
 ggplot(data = ar.pred, aes(x = Date, y = fit)) + 
   mytheme +
-  labs(x = "Year", y = "Frequency (1,000)", title = "Two-year prediction from AR3") +
-  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% confidence interval")) +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "Two-year prediction from AR3") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
   geom_line(aes(y = FluPos, col = "Actual")) +
   geom_line(aes(col = "Predicted"))  + 
   scale_fill_brewer(name = "") + 
@@ -87,17 +71,219 @@ ggplot(data = ar.pred, aes(x = Date, y = fit)) +
   scale_y_continuous(labels = format1000)
 dev.off()
 
-png("Img/drPred.png", width = pltWidth, height = pltHeight)  
-ggplot(data = dr.valid, aes(x = Date, y = fit)) + 
+# dynamic regression prediction
+ts.gft <- ts(can$gft, frequency = 52, start = c(minYr, minWk))
+ts.gft.can.learn <- window(ts.gft, start = c(minYr, minWk), 
+                           end = c(2012, 52))
+ts.gft.can.valid <- window(ts.gft, start = c(2013, 1))
+
+dr.learn.dt <- data.frame(gft = ts.gft.can.learn, FluPos = ts.can.learn)
+dr.valid.dt <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid)
+
+dr33.model <- dynlm(FluPos ~ L(FluPos, 3) + L(gft, 3) + L(FluPos, 52), data = dr.learn.dt)
+dr33.pred <- predict(dr33.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr33.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                       fit = dr33.pred$fit[,"fit"], 
+                       UL = dr33.pred$fit[,"upr"], 
+                       LL = dr33.pred$fit[,"lwr"], 
+                       Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+dr20.model <- dynlm(FluPos ~ L(FluPos, 2) + L(FluPos, 52), data = dr.learn.dt)
+dr20.pred <- predict(dr20.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr20.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr20.pred$fit[,"fit"], 
+                         UL = dr20.pred$fit[,"upr"], 
+                         LL = dr20.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+dr30.model <- dynlm(FluPos ~ L(FluPos, 3) + L(FluPos, 52), data = dr.learn.dt)
+dr30.pred <- predict(dr30.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr30.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr30.pred$fit[,"fit"], 
+                         UL = dr30.pred$fit[,"upr"], 
+                         LL = dr30.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+dr40.model <- dynlm(FluPos ~ L(FluPos, 4) + L(FluPos, 52), data = dr.learn.dt)
+dr40.pred <- predict(dr40.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr40.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr40.pred$fit[,"fit"], 
+                         UL = dr40.pred$fit[,"upr"], 
+                         LL = dr40.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+dr.33plt <- ggplot(data = dr33.valid, aes(x = Date, y = fit)) + 
   mytheme +
-  labs(x = "Year", y = "Frequency (1,000)", title = "Two-year prediction from DR33") +
-  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% confidence interval")) +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR33") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
   geom_line(aes(y = FluPos, col = "Actual")) +
   geom_line(aes(col = "Predicted"))  + 
   scale_fill_brewer(name = "") + 
   scale_color_discrete(name = "") +
   coord_cartesian(ylim = c(-5, 5000)) +
   guides(color = guide_legend(order = 1), 
-       fill = guide_legend(order = 2)) +
+         fill = guide_legend(order = 2)) +
   scale_y_continuous(labels = format1000)
+
+plt.legend <- get_legend(dr.33plt)
+
+dr.20plt <- ggplot(data = dr20.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR20") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000) + theme(legend.position = "none")
+
+dr.30plt <- ggplot(data = dr30.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR30") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000) + theme(legend.position = "none")
+
+dr.40plt <- ggplot(data = dr40.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR40") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000) + theme(legend.position = "none")
+
+png("Img/dr4Preds.png", width = 1.2*pltWidth, height = 1.2*pltHeight)
+grid.arrange(plt.legend, dr.33plt + theme(legend.position = "none"), 
+             dr.20plt, dr.30plt, dr.40plt, 
+             ncol = 2, nrow = 3, layout_matrix = rbind(c(1,1), c(2,3), c(4,5)), 
+             heights = c(0.1, 0.45, 0.45),
+             top = textGrob("Predictions from dynamic regression models", gp=gpar(fontsize=25)))
 dev.off()
+
+
+dr.learn.dt <- data.frame(gft = ts.gft.can.learn, FluPos = ts.can.learn)
+dr.valid.dt <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid)
+
+splitVar <- function(var, name, df.learn, df.valid){
+  tsvar <- ts(var, frequency = 52, start = c(minYr, minWk))
+  
+  tsvar.learn <- window(tsvar, start = c(minYr, minWk), 
+                             end = c(2012, 52))
+  tsvar.valid <- window(tsvar, start = c(2013, 1))
+  
+  df.learn$new <- tsvar.learn
+  df.valid$new <- tsvar.valid
+  
+  names(df.learn)[ncol(df.learn)] <- name
+  names(df.valid)[ncol(df.valid)] <- name
+  
+  return(list(df.learn, df.valid))
+}
+
+tmp <- splitVar(can$FluTest, "FluTest", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$RSVtest, "RSVtest", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$adenot, "adenot", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$parat, "parat", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$RSVpos, "RSVpos", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$adeno, "adeno", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+tmp <- splitVar(can$para, "para", dr.learn.dt, dr.valid.dt)
+dr.learn.dt <- tmp[[1]]
+dr.valid.dt <- tmp[[2]]
+
+dr3a.model <- dynlm(FluPos ~ L(FluPos, 3) + L(FluTest, 3) + L(RSVtest, 3) 
+                    + L(adenot, 3) + L(parat, 3) + L(FluPos, 52), data = dr.learn.dt)
+dr3a.pred <- predict(dr3a.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr3a.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr3a.pred$fit[,"fit"], 
+                         UL = dr3a.pred$fit[,"upr"], 
+                         LL = dr3a.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+ggplot(data = dr3a.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR with multiple regressors 1") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000)
+
+dr3b.model <- dynlm(FluPos ~ L(FluPos, 3) + L(RSVpos, 3) 
+                    + L(adeno, 3) + L(para, 3) + L(FluPos, 52), data = dr.learn.dt)
+dr3b.pred <- predict(dr3b.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr3b.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr3b.pred$fit[,"fit"], 
+                         UL = dr3b.pred$fit[,"upr"], 
+                         LL = dr3b.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+ggplot(data = dr3b.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR with multiple regressors 2") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000)
+
+dr2b.model <- dynlm(FluPos ~ L(FluPos, 2) + L(RSVpos, 2) 
+                    + L(adeno, 2) + L(para, 2) + L(FluPos, 52), data = dr.learn.dt)
+dr2b.pred <- predict(dr2b.model, newdata = dr.valid.dt, interval = "predict", se.fit = TRUE)
+dr2b.valid <- data.frame(gft = ts.gft.can.valid, FluPos = ts.can.valid,
+                         fit = dr2b.pred$fit[,"fit"], 
+                         UL = dr2b.pred$fit[,"upr"], 
+                         LL = dr2b.pred$fit[,"lwr"], 
+                         Date = can$Date[(length(can$Date)-length(ts.can.valid)+1) : length(can$Date)])
+
+ggplot(data = dr2b.valid, aes(x = Date, y = fit)) + 
+  mytheme +
+  labs(x = "Year", y = "Frequency (1,000s)", title = "DR with multiple regressors 2") +
+  geom_ribbon(aes(ymin = LL, ymax = UL, fill = "95% prediction interval")) +
+  geom_line(aes(y = FluPos, col = "Actual")) +
+  geom_line(aes(col = "Predicted"))  + 
+  scale_fill_brewer(name = "") + 
+  scale_color_discrete(name = "") +
+  coord_cartesian(ylim = c(-5, 5000)) +
+  guides(color = guide_legend(order = 1), 
+         fill = guide_legend(order = 2)) +
+  scale_y_continuous(labels = format1000)
